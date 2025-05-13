5b6f7f60651d1beb5430e1ccf14125e39ca6a12e
galibmudasir <mudasirgalib5@gmail.com>
2025-05-13 20:26:32 +0700

    test commit

--- index.php
+++ index.php
@@ -1,26 +1,29 @@
 <!doctype html>
 <html lang="en" data-bs-theme="dark">
+
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
   <title>Arief Store</title>
-<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet">
 
   <!-- Font Awesome 5 -->
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css"
-        crossorigin="anonymous" referrerpolicy="no-referrer">
+    crossorigin="anonymous" referrerpolicy="no-referrer">
 
   <!-- Bootstrap 5 -->
-      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/css/bootstrap.min.css" 
-        rel="stylesheet" 
-        crossorigin="anonymous">
- 
-        <link rel="stylesheet" href="style.css">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/css/bootstrap.min.css"
+    rel="stylesheet"
+    crossorigin="anonymous">
+
+  <link rel="stylesheet" href="style.css">
 </head>
+
 <body>
 
   <?php
+  echo "<h1>Hello world</h1>";
   require_once "includes.php";
   $uri = parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH);
   $uri = trim($uri, "/");
@@ -52,7 +55,8 @@
   ?>
 
   <!-- Bootstrap JS -->
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/js/bootstrap.bundle.min.js" 
-          crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/js/bootstrap.bundle.min.js"
+    crossorigin="anonymous"></script>
 </body>
+
 </html>
